bda78893aeb1b1440c30919d76935fa739e368a0
vitya <vityambik13@gmail.com>
2024-05-22 19:32:10 +0600

    CRUD Application added create/save(refactoring)

--- src/main/java/ru/klishin/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/klishin/springcourse/dao/PersonDAO.java
@@ -11,15 +11,14 @@ public class PersonDAO {
 
     private static int PEOPLE_COUNT;
     private List<Person> people;
+
     {
         people = new ArrayList<>();
 
         people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Jim"));
-        people.add(new Person(++PEOPLE_COUNT, "Kris"));
         people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Klark"));
-
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
     public List<Person> index() {
