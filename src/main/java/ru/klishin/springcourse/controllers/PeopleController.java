32be99e7e0cc2710c3715978c8dbbcc2903a1030
vitya <vityambik13@gmail.com>
2024-05-22 17:33:12 +0600

    CRUD Application added create/save

--- src/main/java/ru/klishin/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/klishin/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.klishin.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.klishin.springcourse.dao.PersonDAO;
+import ru.klishin.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -27,7 +26,18 @@ public class PeopleController {
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("persone", personDAO.show(id));
+        model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
