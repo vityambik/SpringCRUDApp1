f99cda849f2c4306ed02c5f552ad204704095d41
vitya <vityambik13@gmail.com>
2024-06-26 17:18:34 +0600

    CRUD Application added address(validation patterns)

--- src/main/java/ru/klishin/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/klishin/springcourse/dao/PersonDAO.java
@@ -38,12 +38,13 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES (?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES (?, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
     }
 
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(),
-                updatedPerson.getAge(), updatedPerson.getEmail(), id);
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE id=?", updatedPerson.getName(),
+                updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getAddress(), id);
     }
 
     public void delete(int id) {
@@ -94,7 +95,7 @@ public class PersonDAO {
         List<Person> people = new ArrayList<>();
 
         for(int i = 1; i <=1000; i++) {
-            people.add(new Person(i, "Name" + i, 27, "name" + i + "@mail.ru"));
+            people.add(new Person(i, "Name" + i, 27, "name" + i + "@mail.ru", "some address"));
         }
 
         return people;
